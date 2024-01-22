3a7667f7a30c1577fd6fdb8e0535da9e1096a2dc
Matthew Hurley <142431276+Murfew@users.noreply.github.com>
2024-01-22 11:13:21 -0500

    Implemented function to created the grid of squares

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function initializeGrid() {
-    const gridContainer = document.getElementById("grid")
+function initializeGrid(size) {
 
+    const gridContainer = document.getElementById("grid")
     
+    for (let i = 0; i < size; i++) {
+
+        const gridRow = document.createElement("div")
+        gridRow.setAttribute("class", "row")
+        gridContainer.appendChild(gridRow)
+
+
+        for (let j = 0; j < size; j++) {
+
+            const gridItem = document.createElement("div")
+            gridItem.setAttribute("class", "item")
+            gridRow.appendChild(gridItem)
+        }
+    }
 }
+
+initializeGrid(16)
